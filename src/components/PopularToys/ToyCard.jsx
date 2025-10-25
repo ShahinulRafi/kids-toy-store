e816f5259c77186e91e74d1f4acd8da4852cd019
ShahinulRafi <shahinul.csecu@gmail.com>
2025-10-25 14:26:47 +0600

    json data

--- src/components/PopularToys/ToyCard.jsx
+++ src/components/PopularToys/ToyCard.jsx
@@ -6,11 +6,12 @@ import { Link } from "react-router";
 const ToyCard = ({ toy }) => {
   return (
     <div>
-        <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card bg-base-100 w-96 h-96 shadow-sm">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              src={toy.pictureURL}
               alt="Shoes"
+              className="w-full h-52"
             />
           </figure>
           <div className="card-body">
