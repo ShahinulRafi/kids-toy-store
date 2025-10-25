15ce0a514a4869c775965564cf5b5dbcfee29ccc
ShahinulRafi <shahinul.csecu@gmail.com>
2025-10-25 12:30:32 +0600

    footer section done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Footer = () => {
   return (
     <div className="w-full mx-auto">
       <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
         <nav className="grid grid-flow-col gap-4">
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover">
+            <Link to="/">Home</Link>
+          </a>
+          <a className="link link-hover">
+            <Link to="/alltoys">All Toys</Link>
+          </a>
+          <a className="link link-hover">
+            <Link to="/mytoys">My Toys</Link>
+          </a>
         </nav>
         <nav>
           <div className="grid grid-flow-col gap-4">
@@ -49,8 +55,8 @@ const Footer = () => {
         </nav>
         <aside>
           <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by ACME
-            Industries Ltd
+            Copyright © {new Date().getFullYear()} - All right reserved by Kids
+            Toy Store
           </p>
         </aside>
       </footer>
